3136919e710cc58300ca111d5b1d4010dd38d5ec
Sash Zats <sash@zats.io>
2015-05-09 16:20:02 -0700

    Added a framework target for Cartage

---
+++ Swizzler/Swizzler.h
@@ -0,0 +1,19 @@
+//
+//  Swizzler-iOS.h
+//  Swizzler-iOS
+//
+//  Created by Sash Zats on 5/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swizzler-iOS.
+FOUNDATION_EXPORT double Swizzler_iOSVersionNumber;
+
+//! Project version string for Swizzler-iOS.
+FOUNDATION_EXPORT const unsigned char Swizzler_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swizzler_iOS/PublicHeader.h>
+
+#import "WMLSwizzler.h"
